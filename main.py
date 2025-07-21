e8be8abf0a84aed0557a1b2ce3a07594f2671493
Stella <aniretake0910@gmail.com>
2025-07-21 23:22:56 +0300

    Add an option to change the file path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import configargparse
+from dotenv import load_dotenv
 import datetime
 import collections
 import pandas
@@ -7,15 +9,13 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def calculate_the_age():
-    now_year = datetime.datetime.now().year
+def calculate_the_age(current_year):
     year_of_foundation = 1920
-    age_of_the_company = now_year - year_of_foundation
+    age_of_the_company = current_year - year_of_foundation
     return age_of_the_company
 
 
-
-def change_the_word_year(age=calculate_the_age()):
+def change_the_word_year(age):
     if 11 <= age % 100 <= 20:
         return f"{age} лет"
     elif age % 10 == 1:
@@ -26,22 +26,39 @@ def change_the_word_year(age=calculate_the_age()):
         return f"{age} лет"
 
 
+def create_parser():
+    parser = configargparse.ArgParser()
+
+    parser.add_argument(
+        '-p', '--excel_file_path',
+        env_var='WINE_EXCEL_PATH',
+        default='wine3.xlsx'
+    )
+
+    return parser
+
+
 def main():
-    wines_df = pandas.read_excel('wine3.xlsx',
-                                sheet_name='Лист1',
-                                keep_default_na=False)
+    load_dotenv()
+    parser = create_parser()
+    options = parser.parse_args()
+
+    wines_df = pandas.read_excel(
+        options.excel_file_path,
+        sheet_name='Лист1',
+        keep_default_na=False
+        )
 
     sorted_wines_df = wines_df.sort_values('Категория')
 
-    dict_sorted_wines = collections.defaultdict(list)
+    sorted_wines_by_category = collections.defaultdict(list)
 
     for sort in sorted_wines_df.to_dict(orient='records'):
-        dict_sorted_wines[sort['Категория']].append(sort)
+        sorted_wines_by_category[sort['Категория']].append(sort)
 
     groups_of_wines = []
-    for group in dict_sorted_wines.values():
+    for group in sorted_wines_by_category.values():
         groups_of_wines.extend(group)
-
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -50,10 +67,12 @@ def main():
 
     template = env.get_template('template.html')
 
+    current_year = datetime.datetime.now().year
+
     rendered_page = template.render(
-        dict_sorted_wines=dict_sorted_wines,
+        sorted_wines_by_category=sorted_wines_by_category,
         groups_of_wines=groups_of_wines,
-        age=change_the_word_year(calculate_the_age()),
+        age=change_the_word_year(calculate_the_age(current_year)),
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
